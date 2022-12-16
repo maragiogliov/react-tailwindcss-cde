31146b3b250418a4c449edec3b3bc5e442bfd6b2
maragiogliov <maragiogliov@protonmail.com>
2022-12-16 18:18:16 +0100

    forms added

--- src/App.js
+++ src/App.js
@@ -1,15 +1,11 @@
 import './App.css';
-import Navbar from './components/Navbar';
+import '@tailwindcss/forms'
+import Hero from './components/Hero'
 
 function App() {
   return (
     <div className='mx-auto mt-0'>
-      <Navbar/>
-      <h1 className="text-3xl text-center underline">
-        Hello Tailwingcss + React 18.2.0
-        Olive
-      </h1>
-
+      <Hero/>
     </div>
         
  
