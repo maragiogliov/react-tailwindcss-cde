aa33855bb29ab0cc3d5f9b903e249c0be361aaed
maragiogliov <maragiogliov@protonmail.com>
2022-12-16 12:25:08 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -4,9 +4,10 @@ function App() {
   return (
     <h1 className="text-3xl font-bold underline">
       Hello Tailwingcss + React 18.2.0
+      Olive
     </h1>
         
-      <h2  className="text-3xl font-bold underline"> olive <h2>
+ 
   );
 }
 
