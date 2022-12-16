f8b367c9339e618e5ea8b6098aa4aab973a933ed
maragiogliov <maragiogliov@protonmail.com>
2022-12-16 15:19:16 +0100

    navbar

--- src/App.js
+++ src/App.js
@@ -1,11 +1,16 @@
 import './App.css';
+import Navbar from './components/Navbar';
 
 function App() {
   return (
-    <h1 className="text-3xl font-bold underline">
-      Hello Tailwingcss + React 18.2.0
-      Olive
-    </h1>
+    <div className='mx-auto mt-0'>
+      <Navbar/>
+      <h1 className="text-3xl text-center underline">
+        Hello Tailwingcss + React 18.2.0
+        Olive
+      </h1>
+
+    </div>
         
  
   );
