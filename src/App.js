8c150872f2f247715fb1fdcf604d83af51e33462
Victor Maragioglio <72440986+maragiogliov@users.noreply.github.com>
2022-12-16 12:16:45 +0100

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import './App.css';
+
+function App() {
+  return (
+    <h1 className="text-3xl font-bold underline">
+      Hello Tailwingcss + React 18.2.0
+    </h1>
+  );
+}
+
+export default App;
