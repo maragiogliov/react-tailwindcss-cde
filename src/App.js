68113af6119a9a4437eb07b8295a4385365760f0
Victor Maragioglio <72440986+maragiogliov@users.noreply.github.com>
2022-12-16 12:18:56 +0100

    Update App.js

--- src/App.js
+++ src/App.js
@@ -5,6 +5,9 @@ function App() {
     <h1 className="text-3xl font-bold underline">
       Hello Tailwingcss + React 18.2.0
     </h1>
+        <h1 className="text-3xl font-bold underline">
+      <h2> clasName="text-3x1 underline" <h2>
+    </h
   );
 }
 
