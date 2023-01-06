e7ea7086ca0fb9534f3071deb0a1dd9730b5daf2
Victor <maragiogliov@protonmail.com>
2023-01-06 22:17:21 +0100

    GO

--- src/components/Block1/Block1.js
+++ src/components/Block1/Block1.js
@@ -8,7 +8,7 @@ const bottle = new URL ("../../assets/Group 90 1.png",import.meta.url)
       return (
         <>
         
-      <section className='Block1 lg:bg-orange-400'>
+      <section className='Block1'>
           <img className='Block1__img1 ' src={bottle} alt='bottle picture' />
           <img className='Block1__img2 lg:m-11' src={olivesPicture} alt='olives picture' />
       </section>
