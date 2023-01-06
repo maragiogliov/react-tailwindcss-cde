a27036abc00b4dd37d5f2737bb73f17e9c77ea3f
Victor <maragiogliov@protonmail.com>
2023-01-06 20:33:42 +0100

    go

--- src/components/Block3/Block3.js
+++ src/components/Block3/Block3.js
@@ -16,29 +16,21 @@ export default function Block3() {
   return (
       <>
             <section class="overflow-hidden text-gray-700">
-  <div class="container px-5 py-2 mx-auto lg:pt-24 lg:px-32">
+  <div class="container   mx-auto lg:pt-24 lg:px-32">
+    
     <div class=" bg-green-300 flex flex-wrap -m-1 md:-m-2">
-
       <div class=" bg-indigo-300 flex flex-wrap w-1/2">
-      
-     
         <div class="w-full p-1 md:p-2">
           <img alt="gallery" class="block object-cover object-center w-full h-full rounded-lg"
          src={arbolConAlfombra} />
         </div>
       </div>
 
-
       <div class=" bg-yellow-300 flex flex-wrap w-1/2">
         <div class="w-full p-1 md:p-2">
           <img alt="gallery" class="block object-cover object-center w-full h-full rounded-lg"
-             
-               
-                  src={guyPickingOlives} />
-             
+          src={guyPickingOlives} />
         </div>
-
-        
         <div class="w-1/2 p-1 md:p-2">
           <img alt="gallery" class="block object-cover object-center w-full h-full rounded-lg"
             src={arbolArropado } />
@@ -52,6 +44,7 @@ export default function Block3() {
             src="https://mdbcdn.b-cdn.net/img/Photos/Horizontal/Nature/4-col/img%20(77).webp" />
         </div>
       </div>
+
     </div>
   </div>
 </section>
