ac44090967b306aa2d55c66ea4a756857988e251
Victor <maragiogliov@protonmail.com>
2023-01-06 20:24:07 +0100

    go

--- src/components/Block3/Block3.js
+++ src/components/Block3/Block3.js
@@ -2,6 +2,13 @@ import React from 'react'
 import './Block3.css';
 const olivesPicture = new URL ("../../assets/OLIVES DARK GREEN.JPG",import.meta.url)
 const guyPickingOlives = new URL ("../../assets/GUY PICKING OLIVES .jpg",import.meta.url)
+const arbolConAlfombra = new URL ("../../assets/ARBOL CON ALFOMBRA.JPG",import.meta.url)
+const arbolArropado = new URL ("../../assets/ARBOL ARROPADO.JPG",import.meta.url)
+
+
+
+
+
 
 
 
@@ -13,25 +20,11 @@ export default function Block3() {
     <div class=" bg-green-300 flex flex-wrap -m-1 md:-m-2">
 
       <div class=" bg-indigo-300 flex flex-wrap w-1/2">
-        <div class="w-1/2 p-1 md:p-2">
-          <img alt="gallery" class="block object-cover object-center w-full h-full rounded-lg"
-
-
-            src={olivesPicture}
-
-           />
-
-          
-        </div>
-
- 
-        <div class="w-1/2 p-1 md:p-2">
-          <img alt="gallery" class="block object-cover object-center w-full h-full rounded-lg"
-            src={guyPickingOlives} />
-        </div>
+      
+     
         <div class="w-full p-1 md:p-2">
           <img alt="gallery" class="block object-cover object-center w-full h-full rounded-lg"
-            src="https://mdbcdn.b-cdn.net/img/Photos/Horizontal/Nature/4-col/img%20(73).webp" />
+         src={arbolConAlfombra} />
         </div>
       </div>
 
@@ -39,11 +32,16 @@ export default function Block3() {
       <div class=" bg-yellow-300 flex flex-wrap w-1/2">
         <div class="w-full p-1 md:p-2">
           <img alt="gallery" class="block object-cover object-center w-full h-full rounded-lg"
-            src="https://mdbcdn.b-cdn.net/img/Photos/Horizontal/Nature/4-col/img%20(74).webp" />
+             
+               
+                  src={guyPickingOlives} />
+             
         </div>
+
+        
         <div class="w-1/2 p-1 md:p-2">
           <img alt="gallery" class="block object-cover object-center w-full h-full rounded-lg"
-            src="https://mdbcdn.b-cdn.net/img/Photos/Horizontal/Nature/4-col/img%20(75).webp" />
+            src={arbolArropado } />
         </div>
         <div class="w-1/2 p-1 md:p-2">
           <img alt="gallery" class="block object-cover object-center w-full h-full rounded-lg"
