1426714d0f9941330beb1a7257f3314ece10f615
Victor <maragiogliov@protonmail.com>
2023-01-08 18:23:05 +0100

    go

--- src/components/Block7/Block7.js
+++ src/components/Block7/Block7.js
@@ -16,23 +16,31 @@ export default function Block7() {
   
 
         <div class=" bg-black  flex flex-wrap w-1/2">
-          <div class="w-full p-1 md:p-2">
+          <div class="w-full h-1/3 p-1 md:p-2">
             <img alt="gallery" class="block object-cover object-center w-full h-full "
             src={trailer} />
           </div>
          
-          <div class="w-full p-1 md:p-2">
+          <div class="w-full h-1/3 p-1 md:p-2">
             <img alt="gallery" class="block object-cover object-center w-full h-full"
               src={trailerDay } />
           </div>
-          <div class="w-full p-1 md:p-2">
+          <div class="w-full h-1/3 p-1 md:p-2">
             <img alt="gallery" class="block object-cover object-center w-full h-full"
               src={basirico}/>
           </div>
         </div>
       
         <div class=" bg-black flex flex-wrap w-1/2">
-          <div class="w-full p-1 md:p-2">
+          <div class="w-full h-1/3 p-1 md:p-2">
+            <img alt="gallery" class="block object-cover object-center w-full h-full "
+            src={ botellaEnMano} />
+          </div>
+          <div class="w-full h-1/3 p-1 md:p-2">
+            <img alt="gallery" class="block object-cover object-center w-full h-full "
+            src={ botellaEnMano} />
+          </div>
+          <div class="w-full h-1/3 p-1 md:p-2">
             <img alt="gallery" class="block object-cover object-center w-full h-full "
             src={ botellaEnMano} />
           </div>
