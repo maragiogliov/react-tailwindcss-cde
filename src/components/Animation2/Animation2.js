472dbb15cde3936532362426d8be377fb9e859ea
Victor <maragiogliov@protonmail.com>
2023-01-09 23:48:26 +0100

    go

--- src/components/Animation2/Animation2.js
+++ src/components/Animation2/Animation2.js
@@ -15,6 +15,11 @@ export const Animation2 = () => {
     const renderer = new THREE.WebGLRenderer();
     renderer.setSize(window.innerWidth, window.innerHeight);
 
+    // Set the background color of the renderer to white
+    const white = new THREE.Color();
+    white.setHex(0xFFFFFF);
+    renderer.setClearColor(white);
+
     // Enable shadows on the renderer
     renderer.shadowMap.type = THREE.PCFShadowMap;
     renderer.shadowMap.enabled = true;
@@ -32,22 +37,23 @@ export const Animation2 = () => {
     containerRef.current.appendChild(renderer.domElement);
 
     // Create a new Three.js torus knot and add it to the scene
-    const geometry = new THREE.TorusKnotGeometry(100, 20, 10, 160);
-    const material = new THREE.MeshStandardMaterial({ color: 0xffffffb4 });
+    const geometry = new THREE.TorusKnotGeometry(10, 3, 300, 160);
+    // (10, 20, 300, 160)
+    const material = new THREE.MeshStandardMaterial({ color: 0xe0f3cb });
     const figure = new THREE.Mesh(geometry, material);
     figure.castShadow = true;
     figure.receiveShadow = true;
     scene.add(figure);
 
     // Add a light source to the scene
-    const light = new THREE.DirectionalLight(0xffffff, 5);
+    const light = new THREE.DirectionalLight(0xffffff, 1);
     light.position.set(1, 1, 1);
     light.castShadow = true;
     scene.add(light);
 
     // Add a plane to receive shadows
     const planeGeometry = new THREE.PlaneGeometry(5, 5);
-    const planeMaterial = new THREE.MeshStandardMaterial({ color: 0xFFFFF });
+    const planeMaterial = new THREE.MeshStandardMaterial({ color: 0xFFFFFF });
     const plane = new THREE.Mesh(planeGeometry, planeMaterial);
     plane.rotation.x = -Math.PI / 2;
     plane.receiveShadow = true;
@@ -65,11 +71,11 @@ export const Animation2 = () => {
     };
 
     animate();
-
-
   }, []);
 
-  return <div ref={containerRef} className='flex  justify-around ' />;
+  return <div ref={containerRef} className='flex  justify-around h-1' />;
 };
 
 export default Animation2;
+
+
