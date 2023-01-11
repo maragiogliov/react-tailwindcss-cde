7ebdbc3aec8b94e3d3424feff76ce9d99d0daf85
Victor <maragiogliov@protonmail.com>
2023-01-11 22:03:01 +0100

    GO SAVE

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 import './App.css';
 import '@tailwindcss/forms'
 import Hero from './components/Hero/Hero'
-import Contact from './components/Contact/Contact';
 import { I18nProvider, LOCALES } from './i18n';
 import { useState } from 'react';
 import { ReactComponent as GB_flag } from './assets/GB.svg';
@@ -11,21 +10,28 @@ import { ReactComponent as DE_flag } from './assets/DE.svg';
 
 function App() {
   const [locale, setLocale]=useState(LOCALES.ENGLISH)
+
   return (
     <>
     <I18nProvider locale={locale}>
 
       <section className='buttons-language-container'>
-      <button onClick={()=> setLocale(LOCALES.ENGLISH)}>
-      <GB_flag className='buttons_languages'  />
-      </button>
-      <button onClick={()=> setLocale(LOCALES.GERMAN)}>
-      <DE_flag className='buttons_languages'  />
-      </button>
-      <button onClick={()=> setLocale(LOCALES.FRENCH)}>
-      <FR_flag className='buttons_languages'  />
-      </button>
+      <button onClick={()=> 
+          setLocale(LOCALES.ENGLISH)}>
+            <GB_flag className='buttons_languages'/>
+        </button>
+  
+        <button onClick={()=> 
+          setLocale(LOCALES.GERMAN)}>
+            <DE_flag className='buttons_languages'/>
+        </button>
+            
+        <button onClick={()=> 
+          setLocale(LOCALES.FRENCH)}>
+            <FR_flag className='buttons_languages'/>
+        </button>
       </section>
+
         <Hero/>
         {/*<Contact/>*/ }
 
