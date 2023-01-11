3590ff872f86a1ef2ae5923deca95ea1052a7df5
Victor <maragiogliov@protonmail.com>
2023-01-11 21:30:50 +0100

    go languages

--- src/App.js
+++ src/App.js
@@ -4,6 +4,9 @@ import Hero from './components/Hero/Hero'
 import Contact from './components/Contact/Contact';
 import { I18nProvider, LOCALES } from './i18n';
 import { useState } from 'react';
+import { ReactComponent as GB_flag } from './assets/GB.svg';
+import { ReactComponent as FR_flag } from './assets/FR.svg';
+import { ReactComponent as DE_flag } from './assets/DE.svg';
 
 
 function App() {
@@ -13,9 +16,15 @@ function App() {
     <I18nProvider locale={locale}>
 
       <section className='buttons-language-container'>
-      <button onClick={()=> setLocale(LOCALES.ENGLISH)}>English</button>
-      <button onClick={()=> setLocale(LOCALES.GERMAN)}>German</button>
-      <button onClick={()=> setLocale(LOCALES.FRENCH)}>French</button>
+      <button onClick={()=> setLocale(LOCALES.ENGLISH)}>
+      <GB_flag className='buttons_languages'  />
+      </button>
+      <button onClick={()=> setLocale(LOCALES.GERMAN)}>
+      <DE_flag className='buttons_languages'  />
+      </button>
+      <button onClick={()=> setLocale(LOCALES.FRENCH)}>
+      <FR_flag className='buttons_languages'  />
+      </button>
       </section>
         <Hero/>
         {/*<Contact/>*/ }
