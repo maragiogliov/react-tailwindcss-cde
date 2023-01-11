e266a33ccee42c44f73869f7175fd5be32cd47b7
Victor <maragiogliov@protonmail.com>
2023-01-11 20:58:39 +0100

    GO GERMAN

--- src/i18n/messages/de-DE.js
+++ src/i18n/messages/de-DE.js
@@ -5,10 +5,12 @@ export default {
     'hello': 'Hallo',
 
     'Natural Taste of Sicily':'Natürlicher Geschmack Siziliens',
+    'With over 50 years of family farm tradition, CDE elevate your cooking with our organic extra virgin olive oil.':'Mit über 50 Jahren landwirtschaftlicher Familientradition veredelt CDE Ihre Küche mit unserem nativen Bio-Olivenöl extra..',
+    'Shop Olives':'Oliven kaufen',
 
 
-    'With over 50 years of family farm tradition, CDE elevate your cooking with our    organic extra virgin olive oil.':'Mit über 50 Jahren landwirtschaftlicher Familientradition veredelt CDE Ihre Küche mit unserem nativen Bio-Olivenöl extra.',
+
     
-    'Shop Olives':'Oliven kaufen' 
+
  },
 };
